110496b58754379a582a60d1a1d3821e52e82093
Dakoda Willden <dakodaw@yahoo.com>
2020-10-21 08:18:09 -0600

    Add two tests for repeating equals, and executing on any operator

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import TestRenderer from 'react-test-renderer';
+import { ResultComponent } from './result/Result';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('pressing "11 + 1 + 2 + " should have the result 14', () => {
+  //Arrange
+  const testRenderer = TestRenderer.create(<App />);
+  const testInstance = testRenderer.root;
+
+  //Act
+  testInstance.instance.onClick(1);
+  testInstance.instance.onClick(1);
+  testInstance.instance.onClick('+');
+  testInstance.instance.onClick(1);
+  testInstance.instance.onClick('+');
+  testInstance.instance.onClick(2);
+  testInstance.instance.onClick('+');
+
+  var result = testInstance.findByType(ResultComponent).props.result;
+
+  //Assert
+  expect(result).toBe(14);
+});
+
+test('pressing "11 + 1 + 2 = = = " should have the result 18 with the equals button pressed three times, repeating last operation twice', () => {
+  //Arrange
+  const testRenderer = TestRenderer.create(<App />);
+  const testInstance = testRenderer.root;
+
+  //Act
+  testInstance.instance.onClick(1);
+  testInstance.instance.onClick(1);
+  testInstance.instance.onClick('+');
+  testInstance.instance.onClick(1);
+  testInstance.instance.onClick('+');
+  testInstance.instance.onClick(2);
+  testInstance.instance.onClick('=');
+  testInstance.instance.onClick('=');
+  testInstance.instance.onClick('=');
+
+  var result = testInstance.findByType(ResultComponent).props.result;
+  console.log(result)
+
+  //Assert
+  expect(result).toBe(18);
 });
